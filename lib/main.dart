24e9e68d05d526ad6895dec617589be895d2a08f
KavyaParengal <kavyaparengal9037@gmail.com>
2023-08-30 11:48:49 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+import 'staggeredgridexample.dart';
+
+void main(){
+  return runApp(
+      MaterialApp(
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
+      )
+  );
+}
